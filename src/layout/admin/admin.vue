ae1486d6700d0c09b1ae987d48389fbcbcd39947
limyel <limyel@outlook.com>
2025-03-06 13:19:30 +0800

    style(layout): 优化底部样式和布局
    - 在 admin.vue 中添加 .el-footer 样式，设置 padding 为 0
    - 更新 AdminFooter.vue 中的底部内容，增加版权信息和链接
    -调整底部背景颜色、文本样式和对齐方式

--- src/layout/admin/admin.vue
+++ src/layout/admin/admin.vue
@@ -43,4 +43,8 @@ const menuStore = useMenuStore()
 .el-header {
   padding: 0!important;
 }
+
+.el-footer {
+  padding: 0!important;
+}
 </style>
--- src/layout/admin/components/AdminFooter.vue
+++ src/layout/admin/components/AdminFooter.vue
@@ -3,8 +3,9 @@
 </script>
 
 <template>
-  <div class="bg-cyan-700 text-white">
-    底部
+  <div class="bg-white py-5 flex items-center justify-center text-sm text-gray-500 shadow-none">
+    <!-- Copyright 版权信息 -->
+    Copyright © 2025. All rights reserved. Provided by&nbsp; <a class="underline" href="https://limyel.com" target="_blank">limyel</a>
   </div>
 </template>
 
